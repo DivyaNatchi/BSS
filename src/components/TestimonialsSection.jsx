e7bab9d7720de61ff688a893b074b77f34c2f843
Divyalakshmi <ndivyalakshmi88@gmail.com>
2025-01-29 16:13:00 +0300

    step-8 testimonial section updated partially

--- src/components/TestimonialsSection.jsx
+++ src/components/TestimonialsSection.jsx
@@ -7,53 +7,24 @@ import AOS from "aos"; // For animations (if using AOS)
 import "../assets/css/testimonialsSection.css";
 
 // Importing testimonial images
-import testimonialImg1 from "../assets/img/testimonials/testimonials-1.jpg";
-import testimonialImg2 from "../assets/img/testimonials/testimonials-2.jpg";
-import testimonialImg3 from "../assets/img/testimonials/testimonials-3.jpg";
-import testimonialImg4 from "../assets/img/testimonials/testimonials-4.jpg";
-import testimonialImg5 from "../assets/img/testimonials/testimonials-5.jpg";
+import anlo from "../assets/img/testimonials/anlo_logo.png";
+import mac from "../assets/img/testimonials/MAC.jpg";
 
 // Testimonials data
 const testimonials = [
   {
-    name: "Saul Goodman",
+    name: "Annja",
     title: "Ceo & Founder",
-    image: testimonialImg1,
-    rating: 5,
-    feedback:
-      "Proin iaculis purus consequat sem cure digni ssim donec porttitora entum suscipit rhoncus. Accusantium quam, ultricies eget id, aliquam eget nibh et. Maecen aliquam, risus at semper.",
+    image: anlo,
+    feedback: `Byte Size has delivered an application that has rapidly become an invaluable part of how we operate, and they have done so at a highly competitive cost to the business. They understood our frustration with the output from Harvest and developed an innovative solution.`,
+    hasReadMore: true,
   },
   {
-    name: "Sara Wilsson",
-    title: "Designer",
-    image: testimonialImg2,
-    rating: 5,
-    feedback:
-      "Export tempor illum tamen malis malis eram quae irure esse labore quem cillum quid cillum eram malis quorum velit fore eram velit sunt aliqua noster fugiat irure amet legam anim culpa.",
-  },
-  {
-    name: "Jena Karlis",
-    title: "Store Owner",
-    image: testimonialImg3,
-    rating: 5,
-    feedback:
-      "Enim nisi quem export duis labore cillum quae magna enim sint quorum nulla quem veniam duis minim tempor labore quem eram duis noster aute amet eram fore quis sint minim.",
-  },
-  {
-    name: "Matt Brandon",
-    title: "Freelancer",
-    image: testimonialImg4,
-    rating: 5,
-    feedback:
-      "Fugiat enim eram quae cillum dolore dolor amet nulla culpa multos export minim fugiat minim velit minim dolor enim duis veniam ipsum anim magna sunt elit fore quem dolore labore illum veniam.",
-  },
-  {
-    name: "John Larson",
-    title: "Entrepreneur",
-    image: testimonialImg5,
-    rating: 5,
-    feedback:
-      "Quis quorum aliqua sint quem legam fore sunt eram irure aliqua veniam tempor noster veniam enim culpa labore duis sunt culpa nulla illum cillum fugiat legam esse veniam culpa fore nisi cillum quid.",
+    name: "Muthu",
+    title: "Founder",
+    image: mac,
+    feedback: `Partnering with Byte Size IT Solutions was a game changer for us. Their innovative solutions have streamlined our operations and lifted a significant burden off our shoulders. The customized tool they developed has transformed how we manage our inventory and finances. Byte Size’s expertise and commitment stand out, making a substantial impact on our business’s growth trajectory. They truly offer big business solutions for small business budgets`,
+    hasReadMore: false,
   },
 ];
 
@@ -68,10 +39,6 @@ export default function TestimonialsSection() {
     <section id="testimonials" className="testimonials section">
       <div className="container section-title" data-aos="fade-up">
         <h2>Testimonials</h2>
-        <p>
-          Necessitatibus eius consequatur ex aliquid fuga eum quidem sint
-          consectetur velit
-        </p>
       </div>
       <Container data-aos="fade-up">
         {/* Swiper Component for Testimonials */}
@@ -99,16 +66,21 @@ export default function TestimonialsSection() {
                 />
                 <h3>{testimonial.name}</h3>
                 <h4>{testimonial.title}</h4>
-                <div className="stars">
-                  {[...Array(testimonial.rating)].map((_, i) => (
-                    <i key={i} className="bi bi-star-fill"></i>
-                  ))}
-                </div>
                 <p>
                   <i className="bi bi-quote quote-icon-left"></i>
                   {testimonial.feedback}
+                  {testimonial.hasReadMore && (
+                    <i className="bi bi-arrow-right"></i>
+                  )}
                   <i className="bi bi-quote quote-icon-right"></i>
                 </p>
+
+                {/* Conditionally render the "Read More" button based on the flag */}
+                {testimonial.hasReadMore && (
+                  <button className="read-more-btn">
+                    Read More <i className="bi bi-arrow-right"></i>
+                  </button>
+                )}
               </div>
             </SwiperSlide>
           ))}
